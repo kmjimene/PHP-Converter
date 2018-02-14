af87684b06d43d37e65d21e1b02b4f36f551b6a6
Katherine <kmjimene@users.noreply.github.com>
2018-02-13 21:16:01 -0800

    Create units.php

---
+++ units.php
@@ -0,0 +1,29 @@
+<?php 
+// number in pounds we want to convert to kilograms
+$pounds = 140;
+// floating point value for the pound to kilogram conversion
+$lbs_to_kg = 0.453592;
+// use the variables above to calculate pounds multiplied by the kilogram conversion
+$kilograms = $pounds * $lbs_to_kg;
+// display the pounds to kilograms
+echo "Weight: ";
+echo $pounds;
+echo " lb = ";
+echo $kilograms;
+echo " kg";
+
+
+
+// number in miles we want to convert to kilometers
+$miles = 60;
+// floating point value for the mile to kilometer conversion
+$mi_to_km = 1.60934;
+// use the variables above to calculate miles multiplied by the kilometer conversion
+$kilometers = $miles * $mi_to_km;
+// display the miles to kilometers
+echo "Distance: ";
+echo $miles;
+echo " miles = ";
+echo $kilometers;
+echo " km";
+?>
